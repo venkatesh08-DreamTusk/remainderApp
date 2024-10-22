45971ecb72107caa425a192188a8ba03626001fb
venkatesh08-DreamTusk <venkatesh.r@dreamtusk.com>
2024-10-22 16:19:46 +0530

    inital

---
+++ src/main/java/com/remainder/remainderApp/RemainderAppApplication.java
@@ -0,0 +1,13 @@
+package com.remainder.remainderApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemainderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemainderAppApplication.class, args);
+	}
+
+}
